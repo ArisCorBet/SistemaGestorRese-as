11dbc4e0c7ab2dc7ee30bab9c43e4862cb4b79cf
Jossibel <jossibel.perez@unl.edu.ec>
2025-05-17 23:54:08 -0500

    Subiendo cambios desde la rama JOSSIBEL

--- unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/models/Actor.java
+++ unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/models/Actor.java
@@ -1,5 +1,10 @@
 package com.unl.proyectogrupal.base.models;
 
 public class Actor {
-    
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String fechaNacimiento;
+    private String nacionalidad;
+
 }
