7af4d8f2b165e20014eab628c3935b6c38897fb0
ArisCorBet <ariana.cordova@unl.edu.ec>
2025-05-18 15:47:41 -0500

    daosClases

--- unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/controller/dao/dao_models/DaoGeneroPelicula.java
+++ unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/controller/dao/dao_models/DaoGeneroPelicula.java
@@ -1,5 +1,57 @@
 package com.unl.proyectogrupal.base.controller.dao.dao_models;
 
-public class DaoGeneroPelicula {
-    
+import com.unl.proyectogrupal.base.models.GeneroPelicula;
+import com.unl.proyectogrupal.base.controller.dao.AdapterDao;
+
+public class DaoGeneroPelicula extends AdapterDao<GeneroPelicula> {
+    private GeneroPelicula obj;
+
+    public DaoGeneroPelicula() {
+        super(GeneroPelicula.class);
+        // TODO Auto-generated constructor stub
+    }
+
+    public GeneroPelicula getObj() {
+        if (obj == null)
+            this.obj = new GeneroPelicula();
+        return this.obj;
+    }
+
+    public void setObj(GeneroPelicula obj) {
+        this.obj = obj;
+    }
+
+    public Boolean save() {
+        try {
+            obj.setIdGenero(listAll().getLength()+1);
+            this.persist(obj);
+            return true;
+        } catch (Exception e) {
+            //TODO
+            return false;
+            // TODO: handle exception
+        }
+    }
+
+    public Boolean update(Integer pos) {
+        try {
+            this.update(obj, pos);
+            return true;
+        } catch (Exception e) {
+            //TODO
+            return false;
+            // TODO: handle exception
+        }
+    }
+
+    public static void main(String[] args) {
+        DaoGeneroPelicula da = new DaoGeneroPelicula();
+        da.getObj().setIdGenero(da.listAll().getLength() + 1);
+        da.getObj().setIdPelicula("P001");
+        if (da.save())
+            System.out.println("GUARDADO");
+        else
+            System.out.println("Hubo un error");
+    }
+
 }
--- unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/models/GeneroPelicula.java
+++ unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/models/GeneroPelicula.java
@@ -1,5 +1,24 @@
 package com.unl.proyectogrupal.base.models;
 
 public class GeneroPelicula {
-    
+    private Integer idGenero;
+    private String idPelicula;
+
+    public Integer getIdGenero() {
+        return this.idGenero;
+    }
+
+    public void setIdGenero(Integer idGenero) {
+        this.idGenero = idGenero;
+    }
+
+    public String getIdPelicula() {
+        return this.idPelicula;
+    }
+
+    public void setIdPelicula(String idPelicula) {
+        this.idPelicula = idPelicula;
+    }
+
 }
+
