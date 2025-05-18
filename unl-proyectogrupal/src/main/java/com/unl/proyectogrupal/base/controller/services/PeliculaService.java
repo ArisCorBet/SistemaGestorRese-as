5b4a9a913f1c23a1e97e863ea94de1d1b34a7529
IsaKaiser04 <isauro.rivera@unl.edu.ec>
2025-05-18 16:16:00 -0500

    Creacion de Service

---
+++ unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/controller/services/PeliculaService.java
@@ -0,0 +1,89 @@
+package com.unl.proyectogrupal.base.controller.services;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import com.unl.proyectogrupal.base.controller.dao.dao_models.DaoPelicula;
+import com.unl.proyectogrupal.base.models.Pelicula;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.hilla.BrowserCallable;
+
+import jakarta.validation.constraints.NotEmpty;
+
+@BrowserCallable
+@AnonymousAllowed
+
+public class PeliculaService {
+    private DaoPelicula dp;
+
+    public PeliculaService() {
+        this.dp = new DaoPelicula();
+    }
+
+    public void createPelicula(@NotEmpty String titulo,@NotEmpty String sinopsis, Integer duracion,@NotEmpty String trailer, Date fechaEsteno, Integer idGenero ) throws Exception {
+        if (titulo.trim().length() > 0 && sinopsis.trim().length() > 0 && trailer.trim().length() > 0 && duracion > 0 && idGenero > 0) {
+            dp.getObj().setTitulo(titulo);
+            dp.getObj().setSinopsis(sinopsis);
+            dp.getObj().setDuracion(duracion);
+            dp.getObj().setTrailer(trailer);
+            dp.getObj().setFechaEstreno(fechaEsteno);
+            dp.getObj().setIdGenero(idGenero);
+
+            if (!dp.save())
+                throw new Exception("No se pudo guardar los datos de la Pelicula");
+        }
+    }
+    public void updatePelicula(Integer id,@NotEmpty String titulo,@NotEmpty String sinopsis, Integer duracion,@NotEmpty String trailer, Date fechaEsteno, Integer idGenero ) throws Exception {
+        if (titulo.trim().length() > 0 && sinopsis.trim().length() > 0 && trailer.trim().length() > 0 && duracion > 0 && idGenero > 0) {
+            dp.setObj(dp.listAll().get(id - 1));            dp.getObj().setTitulo(titulo);
+            dp.getObj().setSinopsis(sinopsis);
+            dp.getObj().setDuracion(duracion);
+            dp.getObj().setTrailer(trailer);
+            dp.getObj().setFechaEstreno(fechaEsteno);
+            dp.getObj().setIdGenero(idGenero);
+    
+            if (!dp.update(id -1))
+                throw new Exception("No se pudo guardar los datos de la Pelicula");
+        }
+    }
+
+    
+    /*public List<HashMap> listaAlbumGenero() {
+        List<HashMap> lista = new ArrayList<>();
+        DaoGenero da = new DaoGenero();
+        if(!da.listAll().isEmpty()) {
+            Genero [] arreglo = da.listAll().toArray();
+            for(int i = 0; i < arreglo.length; i++) {
+                HashMap<String, String> aux = new HashMap<>();
+                aux.put("value", arreglo[i].getId().toString(i)); 
+                aux.put("label", arreglo[i].getNombre()); 
+                lista.add(aux);  
+            }
+        }
+        return lista;
+    }*/
+
+    public List<HashMap> listPelicula(){
+        List<HashMap> lista = new ArrayList<>();
+        if(!dp.listAll().isEmpty()) {
+            Pelicula [] arreglo = dp.listAll().toArray();
+      
+            for(int i = 0; i < arreglo.length; i++) {
+                HashMap<String, String> aux = new HashMap<>();
+                aux.put("id", String.valueOf(arreglo[i].getId().toString()));
+                aux.put("titulo", arreglo[i].getTitulo());
+                aux.put("sinopsis", arreglo[i].getSinopsis());
+                aux.put("duracion", String.valueOf(arreglo[i].getDuracion()));
+                aux.put("trailer", arreglo[i].getTrailer());
+                aux.put("fechaEstreno", String.valueOf(arreglo[i].getFechaEstreno()));
+                aux.put("idGenero", String.valueOf(arreglo[i].getIdGenero()));
+                lista.add(aux);
+            }
+        }
+        return lista;
+    
+    }
+
+    
+}
