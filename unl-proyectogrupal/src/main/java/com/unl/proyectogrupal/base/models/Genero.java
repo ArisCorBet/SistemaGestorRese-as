16cc3857bcb7521863592e50842abea3eefd7df3
ArisCorBet <ariana.cordova@unl.edu.ec>
2025-05-18 00:53:09 -0500

    daos

--- unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/models/Genero.java
+++ unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/models/Genero.java
@@ -2,7 +2,23 @@ package com.unl.proyectogrupal.base.models;
 
 public class Genero {
     private Integer idGenero;
-    private Stringe nombre;
+    private String nombre;
+
+    public Integer getIdGenero() {
+        return this.idGenero;
+    }
+
+    public void setIdGenero(Integer idGenero) {
+        this.idGenero = idGenero;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     
-    
+
 }
