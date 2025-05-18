25795a87be4139094fe2e55ee8f7aba9c87f9439
Jossibel <jossibel.perez@unl.edu.ec>
2025-05-18 17:43:02 -0500

    Actualización de modelos y creación de DAOs: Actor, Director, RepartoPelicula, DireccionPelicula

--- unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/models/Actor.java
+++ unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/models/Actor.java
@@ -1,10 +1,33 @@
 package com.unl.proyectogrupal.base.models;
 
 public class Actor {
-    private String nombre;
-    private String apellido;
-    private String dni;
-    private String fechaNacimiento;
-    private String nacionalidad;
+
+    private int idActor;
+    private String descripcion;
+    private int anioCarrera;
+
+    public int getIdActor() {
+        return this.idActor;
+    }
+
+    public void setIdActor(int idActor) {
+        this.idActor = idActor;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getAnioCarrera() {
+        return this.anioCarrera;
+    }
+
+    public void setAnioCarrera(int anioCarrera) {
+        this.anioCarrera = anioCarrera;
+    }
 
 }
--- unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/models/DireccionPelicula.java
+++ unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/models/DireccionPelicula.java
@@ -1,5 +1,24 @@
 package com.unl.proyectogrupal.base.models;
 
 public class DireccionPelicula {
+    private int idDirector;
+    private int idPelicula;
+
+    public int getIdDirector() {
+        return this.idDirector;
+    }
+
+    public void setIdDirector(int idDirector) {
+        this.idDirector = idDirector;
+    }
+
+    public int getIdPelicula() {
+        return this.idPelicula;
+    }
+
+    public void setIdPelicula(int idPelicula) {
+        this.idPelicula = idPelicula;
+    }
+
     
 }
