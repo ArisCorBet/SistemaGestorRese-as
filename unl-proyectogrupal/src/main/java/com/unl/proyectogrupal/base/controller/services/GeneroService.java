778df3b9d52d76d7ecaef2a094a067ff60815293
ArisCorBet <ariana.cordova@unl.edu.ec>
2025-05-19 12:22:03 -0500

    services

---
+++ unl-proyectogrupal/src/main/java/com/unl/proyectogrupal/base/controller/services/GeneroService.java
@@ -0,0 +1,74 @@
+package com.unl.proyectogrupal.base.controller.services;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.unl.proyectogrupal.base.controller.dao.dao_models.DaoGenero;
+import com.unl.proyectogrupal.base.models.Genero;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.hilla.BrowserCallable;
+
+import jakarta.validation.constraints.NotEmpty;
+
+@BrowserCallable
+@AnonymousAllowed
+
+public class GeneroService {
+    private DaoGenero dp;
+
+    public GeneroService() {
+        this.dp = new DaoGenero();
+    }
+
+    public void createGenero(@NotEmpty String Nombre) throws Exception {
+        if (Nombre.trim().length() > 0 ) {
+            dp.getObj().setNombre(Nombre);
+
+            if (!dp.save())
+                throw new Exception("No se pudo guardar los datos de la Genero");
+        }
+    }
+    public void updateGenero(Integer idGenero,@NotEmpty String Nombre ) throws Exception {
+        if (Nombre.trim().length() > 0 ) {
+            dp.getObj().setIdGenero(idGenero);
+            dp.getObj().setNombre(Nombre);   
+            if (!dp.update(idGenero -1))
+                throw new Exception("No se pudo guardar los datos de la Genero");
+        }
+    }
+
+    
+    /*public List<HashMap> listaAlbumGenero() {
+        List<HashMap> lista = new ArrayList<>();
+        DaoGenero da = new DaoGenero();
+        if(!da.listAll().isEmpty()) {
+            Genero [] arreglo = da.listAll().toArray();
+            for(int i = 0; i < arreglo.length; i++) {
+                HashMap<String, String> aux = new HashMap<>();
+                aux.put("value", arreglo[i].getId().toString(i)); 
+                aux.put("label", arreglo[i].getNombre()); 
+                lista.add(aux);  
+            }
+        }
+        return lista;
+    }*/
+
+    public List<HashMap> listGenero(){
+        List<HashMap> lista = new ArrayList<>();
+        if(!dp.listAll().isEmpty()) {
+            Genero [] arreglo = dp.listAll().toArray();
+      
+            for(int i = 0; i < arreglo.length; i++) {
+                HashMap<String, String> aux = new HashMap<>();
+                aux.put("idGenero", String.valueOf(arreglo[i].getIdGenero().toString()));
+                aux.put("Nombre", String.valueOf(arreglo[i].getNombre()));
+                lista.add(aux);
+            }
+        }
+        return lista;
+    
+    }
+
+    
+}
