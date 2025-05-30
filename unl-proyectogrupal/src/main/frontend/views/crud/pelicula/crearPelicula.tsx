37a1ecf85e097b07fe2cbaed37a48d72f72e6e08
IsaKaiser04 <isauro.rivera@unl.edu.ec>
2025-05-30 12:08:45 -0500

    Carpetas de Vistas CRUD

---
+++ unl-proyectogrupal/src/main/frontend/views/crud/pelicula/crearPelicula.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Definimos un componente funcional llamado HolaMundo
+const crearPelicula: React.FC = () => {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <p>Este es mi primer componente en TSX</p>
+    </div>
+  );
+};
+
+export default crearPelicula;
